e4868759689c78192d2204948215aaa57eee48fb
IvanShterev <122817920+IvanShterev@users.noreply.github.com>
2023-01-27 09:46:33 +0200

    Add files via upload

---
+++ bitcoin.js
@@ -0,0 +1,18 @@
+
+const outputEl = document.querySelector('.output');
+const btnEl = document.querySelector('.btn');
+
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+async function GetPrice(){
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        outputEl.innerText = data.bpi.EUR.rate + 'EUR';
+        console.log(data.bpi.EUR.rate);
+    } catch (error) {
+        outputEl.innerText = 'Not working, try again later!';
+    }
+}
+
+btnEl.addEventListener('click', GetPrice);
